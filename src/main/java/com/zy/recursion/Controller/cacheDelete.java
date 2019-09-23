ae3a2af9dad34fd73fc7b47984e44f31156d9559
zhangyi <13513505058@163.com>
2019-09-23 22:05:25 +0800

    new

--- src/main/java/com/zy/recursion/Controller/cacheDelete.java
+++ src/main/java/com/zy/recursion/Controller/cacheDelete.java
@@ -1,21 +1,17 @@
 package com.zy.recursion.Controller;
 
-
 import com.zy.recursion.entity.cache;
 import com.zy.recursion.entity.device;
 import com.zy.recursion.entity.returnMessage;
 import com.zy.recursion.service.cache.cacheService;
 import com.zy.recursion.service.device.deviceService;
-import com.zy.recursion.service.node.nodeService;
 import com.zy.recursion.util.ConnectLinuxCommand;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.io.IOException;
-
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -26,7 +22,6 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/cache", method = RequestMethod.GET)
 public class cacheDelete {
-
 
     @Autowired
     private deviceService deviceService;
@@ -48,25 +43,30 @@ public class cacheDelete {
         device device = deviceService.selectDeviceByIp(ip);
         String usrName = device.getDeviceUserName();
         String pwd = device.getDevicePwd();
-        String[] cmd = {"./../home/fnii/handle_cache/bin/cmdsh3 127.0.0.1 15000 2 \"cache delete " + handle + "\""};
-        logger.info("在设备" + ip + "上执行" + cmd[0]);
-        String[] result = ConnectLinuxCommand.execute(ip, usrName, pwd, cmd);
-        if (result != null) {
-            String result2 = result[0].split(" ")[3];
-            String result1 = result2.substring(0, result2.length() - 1);
-            logger.info("删除单条记录结果：" + result1);
-            Date date = new Date();
-            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-            String deleteTime = dateFormat.format(date);
-            Timestamp ts = Timestamp.valueOf(deleteTime);
-            cache.setDeleteTime(ts);
-            cache.setDeleteOperation("删除单条标识");
-            cache.setHandle(handle);
-            cache.setDeleteResult(result1);
-            cacheService.addCache(cache);
-            logger.info("记录存储成功");
-            return result1;
-        } else {
+        String type = device.getDeviceType();
+        if(type.equals("缓存")){
+            String[] cmd = {"./../home/fnii/handle_cache/bin/cmdsh3 127.0.0.1 15000 2 \"cache delete " + handle + "\""};
+            logger.info("在设备" + ip + "上执行" + cmd[0]);
+            String[] result = ConnectLinuxCommand.execute(ip, usrName, pwd, cmd);
+            if (result != null) {
+                String result2 = result[0].split(" ")[3];
+                String result1 = result2.substring(0, result2.length() - 1);
+                logger.info("删除单条记录结果：" + result1);
+                Date date = new Date();
+                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+                String deleteTime = dateFormat.format(date);
+                Timestamp ts = Timestamp.valueOf(deleteTime);
+                cache.setDeleteTime(ts);
+                cache.setDeleteOperation("删除单条标识");
+                cache.setHandle(handle);
+                cache.setDeleteResult(result1);
+                cacheService.addCache(cache);
+                logger.info("记录存储成功");
+                return result1;
+            } else {
+                return null;
+            }
+        }else {
             return null;
         }
     }
@@ -84,37 +84,43 @@ public class cacheDelete {
         device device = deviceService.selectDeviceByIp(ip);
         String usrName = device.getDeviceUserName();
         String pwd = device.getDevicePwd();
-        List result1 = new ArrayList();
-        JSONObject jsonObject1 = new JSONObject();
-        Date date = new Date();
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-        String deleteTime = dateFormat.format(date);
-        Timestamp ts = Timestamp.valueOf(deleteTime);
-        String[] cmd = new String[handle1.length];
-        for (int i = 0; i < handle1.length; i++) {
-            cmd[i] = "./../home/fnii/handle_cache/bin/cmdsh3 127.0.0.1 15000 2 \"cache delete " + handle1[i] + "\"";
-        }
-        String[] result = ConnectLinuxCommand.execute(ip, usrName, pwd, cmd);
-        if (result != null) {
+        String type = device.getDeviceType();
+        if(type.equals("缓存")){
+            List result1 = new ArrayList();
+            JSONObject jsonObject1 = new JSONObject();
+            Date date = new Date();
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+            String deleteTime = dateFormat.format(date);
+            Timestamp ts = Timestamp.valueOf(deleteTime);
+            String[] cmd = new String[handle1.length];
             for (int i = 0; i < handle1.length; i++) {
-                String m = result[i].split(" ")[3];
-                if (m.substring(0, m.length() - 1).equals("ok")) {
-                    jsonObject1.put(handle1[i], "ok");
-                } else {
-                    jsonObject1.put(handle1[i], "false");
-                }
+                cmd[i] = "./../home/fnii/handle_cache/bin/cmdsh3 127.0.0.1 15000 2 \"cache delete " + handle1[i] + "\"";
             }
-            logger.info(result1.toString());
-            cache.setDeleteTime(ts);
-            cache.setDeleteOperation("删除多条标识");
-            cache.setHandle(handle);
-            cache.setDeleteResult(result1.toString());
-            cacheService.addCache(cache);
-            logger.info("记录存储成功");
-            return jsonObject1.toString();
-        } else {
+            String[] result = ConnectLinuxCommand.execute(ip, usrName, pwd, cmd);
+            if (result != null) {
+                for (int i = 0; i < handle1.length; i++) {
+                    String m = result[i].split(" ")[3];
+                    if (m.substring(0, m.length() - 1).equals("ok")) {
+                        jsonObject1.put(handle1[i], "ok");
+                    } else {
+                        jsonObject1.put(handle1[i], "false");
+                    }
+                }
+                logger.info(result1.toString());
+                cache.setDeleteTime(ts);
+                cache.setDeleteOperation("删除多条标识");
+                cache.setHandle(handle);
+                cache.setDeleteResult(result1.toString());
+                cacheService.addCache(cache);
+                logger.info("记录存储成功");
+                return jsonObject1.toString();
+            } else {
+                return null;
+            }
+        }else {
             return null;
         }
+
     }
 
     @CrossOrigin
@@ -127,29 +133,35 @@ public class cacheDelete {
         device device = deviceService.selectDeviceByIp(ip);
         String usrName = device.getDeviceUserName();
         String pwd = device.getDevicePwd();
-        String[] cmd = {"./../home/fnii/handle_cache/bin/cmdsh3 127.0.0.1 15000 2 \"cache flush_all\""};
-        logger.info("在设备" + ip + "上执行" + cmd[0]);
-        String result = ConnectLinuxCommand.execute(ip, usrName, pwd, cmd)[0];
-        if (result != null) {
-            logger.info("删除设备" + ip + "缓存：" + result);
-            Date date = new Date();
-            cache cache = new cache();
-            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-            String deleteTime = dateFormat.format(date);
-            Timestamp ts = Timestamp.valueOf(deleteTime);
-            cache.setHandle("全部");
-            cache.setDeleteOperation("删除" + ip + "对应设备的缓存");
-            cache.setDeleteTime(ts);
-            cache.setDeleteResult(result);
-            cacheService.addCache(cache);
-            returnMessage.setStatus(0);
-            returnMessage.setMessage("删除成功");
-            return returnMessage;
-        } else {
-            returnMessage.setStatus(1);
-            returnMessage.setMessage("服务器连接失败");
-            return returnMessage;
+        String type = device.getDeviceType();
+        if(type.equals("缓存")){
+            String[] cmd = {"./../home/fnii/handle_cache/bin/cmdsh3 127.0.0.1 15000 2 \"cache flush_all\""};
+            logger.info("在设备" + ip + "上执行" + cmd[0]);
+            String result = ConnectLinuxCommand.execute(ip, usrName, pwd, cmd)[0];
+            if (result != null) {
+                logger.info("删除设备" + ip + "缓存：" + result);
+                Date date = new Date();
+                cache cache = new cache();
+                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+                String deleteTime = dateFormat.format(date);
+                Timestamp ts = Timestamp.valueOf(deleteTime);
+                cache.setHandle("全部");
+                cache.setDeleteOperation("删除" + ip + "对应设备的缓存");
+                cache.setDeleteTime(ts);
+                cache.setDeleteResult(result);
+                cacheService.addCache(cache);
+                returnMessage.setStatus(0);
+                returnMessage.setMessage("删除成功");
+                return returnMessage;
+            } else {
+                returnMessage.setStatus(1);
+                returnMessage.setMessage("服务器连接失败");
+                return returnMessage;
+            }
+        }else {
+            return null;
         }
+
     }
 
     @CrossOrigin
@@ -165,14 +177,20 @@ public class cacheDelete {
             String ip = device.getDeviceIp();
             String usrName = device.getDeviceUserName();
             String pwd = device.getDevicePwd();
-            String[] cmd = {"./../home/fnii/handle_cache/bin/cmdsh3 127.0.0.1 15000 2 \"cache flush_all\""};
-            String[] result = ConnectLinuxCommand.execute(ip, usrName, pwd, cmd);
-            if (result != null) {
-                sucList.add(ip);
-                logger.info("删除设备" + ip + "缓存结果：" + result[0]);
-            } else {
+            String type = device.getDeviceType();
+            if (type.equals("缓存")){
+                String[] cmd = {"./../home/fnii/handle_cache/bin/cmdsh3 127.0.0.1 15000 2 \"cache flush_all\""};
+                String[] result = ConnectLinuxCommand.execute(ip, usrName, pwd, cmd);
+                if (result != null) {
+                    sucList.add(ip);
+                    logger.info("删除设备" + ip + "缓存结果：" + result[0]);
+                } else {
+                    falseList.add(ip);
+                    logger.info(ip + "连接失败");
+                }
+            }else {
                 falseList.add(ip);
-                logger.info(ip + "连接失败");
+                logger.info(ip + "不是缓存服务器");
             }
         }
         Date date = new Date();
@@ -196,7 +214,6 @@ public class cacheDelete {
         String startTime = jsonObject.getString("startTime");
         String endTime = jsonObject.getString("endTime");
         return cacheService.selectCache(startTime,endTime);
-
     }
 
 
